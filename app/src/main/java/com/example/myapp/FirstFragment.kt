d91baa7a25cda8b6e2eec931a95d75b8f4a3b3c4
BAG0N <alpenid2004@gmail.com>
2023-01-18 11:36:27 +0400

    add notifications

--- app/src/main/java/com/example/myapp/FirstFragment.kt
+++ app/src/main/java/com/example/myapp/FirstFragment.kt
@@ -1,6 +1,9 @@
 package com.example.myapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +12,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
@@ -27,6 +32,7 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -39,9 +45,19 @@ class FirstFragment : Fragment() {
         FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
 
         view.findViewById<Button>(R.id.btnSend).setOnClickListener {
+            val channel = NotificationChannel("channel1", "channel", NotificationManager.IMPORTANCE_HIGH)
+            val manager = requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
             val title = view.findViewById<EditText>(R.id.etTitle).text.toString()
             val message = view.findViewById<EditText>(R.id.etMessage).text.toString()
             val recipientToken = view.findViewById<EditText>(R.id.etToken).text.toString()
+            val noti = NotificationCompat.Builder(requireActivity(), "channel1")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+            manager.notify(101, noti)
             if(title.isNotEmpty() && message.isNotEmpty() && recipientToken.isNotEmpty()) {
                 PushNotification(
                     NotificationData(title, message),
